56704c8f5afcfd0257e6d3556f9a35a32a8e78ec
arran <arrwhidev@gmail.com>
2015-10-28 17:32:34 +0000

    tidying

--- store/WebDocsStore.js
+++ store/WebDocsStore.js
@@ -2,15 +2,5 @@ import { createStore } from 'redux';
 import reducers from '../reducers';
 
 export default function configureStore(initialState) {
-  const store = createStore(reducers, initialState);
-
-    // if (module.hot) {
-    //   // Enable Webpack hot module replacement for reducers
-    //   module.hot.accept('../reducers', () => {
-    //     const nextReducer = require('../reducers');
-    //     store.replaceReducer(nextReducer);
-    //   });
-    // }
-
-    return store;
+    return createStore(reducers, initialState);
 }
